cdfa6d7a3fefce3fc8dc3368108dbe07fef4ed8d
mayank140905 <mayank140905@gmail.com>
2026-06-24 14:29:14 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        for(char x: s){
+            if(x == '(' || x== '[' || x=='{'){
+                st.push(x);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(x == ')' && st.top() != '('){
+                    return false;
+                }
+                if(x == ']' && st.top() != '['){
+                    return false;
+                }
+                if(x == '}' && st.top() != '{'){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
